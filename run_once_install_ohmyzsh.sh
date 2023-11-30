d089eae34e60eaa25cc82d09c377b86f3506c483
Logan Donley <hello@logandonley.com>
2023-11-30 10:23:36 -0500

    Setting up ohmyzsh

---
+++ run_once_install_ohmyzsh.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+	echo "Getting ohmyz.sh"
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc --skip-chsh
+else
+	echo ".oh-my-zsh already found, skipping."
+fi
