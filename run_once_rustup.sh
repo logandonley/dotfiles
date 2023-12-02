8fa0642464cfdba4cf79529352a63424a49ce3b7
Logan Donley <hello@logandonley.com>
2023-12-02 13:08:45 -0500

    Fixing source issue

--- run_once_rustup.sh
+++ run_once_rustup.sh
@@ -3,6 +3,9 @@
 # Install rust
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
+source $HOME/.cargo/env
+source ~/.profile
+
 # Install cargo binstall
 curl -L --proto '=https' --tlsv1.2 -sSf https://raw.githubusercontent.com/cargo-bins/cargo-binstall/main/install-from-binstall-release.sh | bash
 
